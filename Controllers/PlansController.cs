f4b0320a34f2ee127034e366f2f84d5f6e1815ae
karim12kh <karim236@gmail.com>
2026-05-17 21:09:51 +0200

    Add project files.

---
+++ Controllers/PlansController.cs
@@ -0,0 +1,36 @@
+﻿using GemMangement.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GymMangement.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext dbContext;
+        public PlansController()
+        {
+            dbContext = new GymDbContext();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var plans = await dbContext.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await dbContext.Plans.FindAsync(id);
+
+            if (plan is null)
+            {
+                return RedirectToAction(nameof(Index));
+
+            }
+            else
+            {
+                return View(plan);
+            }
+        }
+    }
+}
---
+++ DbContexts/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GemMangement.Configurations;
+using GemMangement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GemMangement.DbContexts
+{
+    public class GymDbContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=GemMangement;Trusted_Connection=true; TrustServerCertificate=true");
+        }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PlanConfiguration());
+        }
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
